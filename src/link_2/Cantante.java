21257e3ede0b74f0832105e6643c290ddb74fd44
USUARIO <USUARIO@DESKTOP-C89PVFK>
2022-11-12 10:00:05 -0500

    SEMANA10_PRACTICA

---
+++ src/link_2/Cantante.java
@@ -0,0 +1,37 @@
+/*
+ *Clase Cantante
+ */
+package link_2;
+ 
+/**
+ *
+ * @author jugarenequipo-ad
+ */
+ 
+public class Cantante {
+ 
+    //Propiedades de la clase
+    private String nombre;
+ 
+    //Método constructor vacío
+    public Cantante() {
+    }
+ 
+    //Método constructor con parámetros
+    public Cantante(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    //Método para obtener el nombre del cantante
+    public String getNombre() {
+        return nombre;
+    }
+ 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }  
+ 
+    public String devolverCantante(){
+    return "Nombre: " + nombre;
+    }
+}
